c95babe6f8ca59eda0e29bfb14ab45ba66578100
Evan Daniel Mikrut <mikrutevan@gmail.com>
2023-09-27 20:43:12 -0400

    Revert "Initial commit"
    This reverts commit dd7fef1e1c6baf5f9516bfece004906c420eec1f.

--- src/components/Video/Video.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import './Video.css';
-import { Video as VideoModel } from '../../video.jsx'
-
-export default function Video (video){
-    return <div className='video'>
-        <video  src={video.video}></video>
-    </div>
-}
